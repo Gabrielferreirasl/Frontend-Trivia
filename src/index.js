0eb9f8403e6a49c3aea9b6e1013133d86af88226
Gabriel <gabrielferreiradsl@hotmail.com>
2021-12-20 20:33:05 -0300

    trying to fix gh-pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={ store } basename={ process.env.PUBLIC_URL }>
+  <Provider store={ store } basename="/React">
     <BrowserRouter>
       <App />
     </BrowserRouter>
